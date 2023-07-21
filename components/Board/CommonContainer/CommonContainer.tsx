16a91024ea816699aa1b0d61d60eedfbd580699b
syazell1 <mafty.erin01@gmail.com>
2023-07-21 19:26:05 +0800

    add read of notes/todos

--- components/Board/CommonContainer/CommonContainer.tsx
+++ components/Board/CommonContainer/CommonContainer.tsx
@@ -1,16 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react";
 import PublicTodo from "../PublicTodo";
 import Categories from "./Categories";
+import { supabase_client } from "@/db/supabase";
+import { Database } from "@/lib/db.types";
+
+type NoteType = Database["public"]["Tables"]["notes"]["Row"]
 
 export default function CommonContainer() {
+    const [notes, setNotes] = useState<NoteType[] | null>([]);
+
+    useEffect(() => {
+      getTodos();
+    }, []);
+
+    async function getTodos() {
+      const { data } = await supabase_client.from("notes").select();
+      setNotes(data);
+    }
+
     return (
         <div>
             <Categories />
             <div className="flex flex-wrap">
-                <PublicTodo />
-                <PublicTodo />
-                <PublicTodo />
-                <PublicTodo />
-                <PublicTodo />
+                {notes?.map(v => {
+                    return <PublicTodo key={v.id} {...v} />
+                })}
             </div>
         </div>
     )
