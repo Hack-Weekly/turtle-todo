bc96368073139c7bbcefd3e956c75b11e13fa983
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-07-24 17:56:00 +0300

    refactored supabase code

--- components/Board/CommonContainer/CommonContainer.tsx
+++ components/Board/CommonContainer/CommonContainer.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
 import PublicTodo from "../PublicTodo";
@@ -6,35 +6,28 @@ import Categories from "./Categories";
 import { supabase_client } from "@/db/supabase";
 import { Database } from "@/lib/db.types";
 
-type NoteType = Database["public"]["Tables"]["notes"]["Row"]
+type NoteType = Database["public"]["Tables"]["todos"]["Row"];
 
 export default function CommonContainer() {
-    // const [notes, setNotes] = useState<NoteType[] | null>([]);
+  const [todos, setTodos] = useState<NoteType[] | null>([]);
 
-    // useEffect(() => {
-    //   getTodos();
-    // }, []);
+  useEffect(() => {
+    getTodos();
+  }, []);
 
-    // async function getTodos() {
-    //   const { data } = await supabase_client.from("notes").select();
-    //   setNotes(data);
-    // }
-    const data={
-        id: "1",
-        title: "title",
-        created_at: new Date().toISOString(),
-        priority: "high",
-        status: "done",
-    } satisfies NoteType;
-    return (
-        <div>
-            <Categories />
-            <div className="flex flex-wrap">
-                {/* {notes?.map(v => {
-                    return <PublicTodo key={v.id} {...v} />
-                })} */}
-                 <PublicTodo  {...data} />
-            </div>
-        </div>
-    )
+  async function getTodos() {
+    const { data } = await supabase_client.from("todos").select();
+    setTodos(data);
+  }
+
+  return (
+    <div>
+      <Categories />
+      <div className="flex flex-wrap">
+        {todos?.map((v) => {
+          return <PublicTodo key={v.id} {...v} />;
+        })}
+      </div>
+    </div>
+  );
 }
