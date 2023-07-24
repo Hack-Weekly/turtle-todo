7f7f99fa10b1c3f4149784aa70380d7bb5c33860
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-07-24 15:32:58 +0300

    added form component from shadcn

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -7,5 +7,12 @@ export default async function UpdateTodo({
 }) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  return <div>UpdateTodo</div>;
+  return (
+    <div className="container">
+        <h2 className="scroll-m-20 py-8 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+          Update Todo
+        </h2>
+        
+    </div>
+  );
 }
