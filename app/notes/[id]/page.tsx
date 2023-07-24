5ee62dad9809866e451ab781ffe504308c2d2a84
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-07-24 15:36:59 +0300

    Revert "added form component from shadcn"
    This reverts commit 6e24da1c8c37185d994e0d02d84c81d79d2b5e3d.

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -7,12 +7,5 @@ export default async function UpdateTodo({
 }) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  return (
-    <div className="container">
-        <h2 className="scroll-m-20 py-8 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-          Update Todo
-        </h2>
-        
-    </div>
-  );
+  return <div>UpdateTodo</div>;
 }
