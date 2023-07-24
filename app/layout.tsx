9d30c98de0fa0c497cbe71656f0542fd56d9218e
syazell1 <mafty.erin01@gmail.com>
2023-07-24 22:16:21 +0800

    add toaster context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import "./globals.css";
 import { Inter } from "next/font/google";
 export const metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <body>
+        <Toaster />
         <main>{children}</main>
       </body>
     </html>
